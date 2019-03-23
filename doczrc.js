05c0398af429d0214f7aa75d17d47c2854ad52aa
Albert <albertgaohy@gmail.com>
2019-03-24 00:59:11 +1300

    Use now.sh to manage the docs

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,5 @@ module.exports = {
   title: 'Docz Typescript',
   typescript: true,
   public: '/assets',
-  src: './documentation',
-  dest: './docs',
-  base: '/react-oxygen-ui/',
+  dest: './release-docs',
 };
